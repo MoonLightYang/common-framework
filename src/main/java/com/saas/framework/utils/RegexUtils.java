2a5d5e719b6821d030f75b30f14538de67c6fa39
MoonLightYang <729355767@qq.com>
2021-01-21 11:17:01 +0800

    正则重置

--- src/main/java/com/saas/framework/utils/RegexUtils.java
+++ src/main/java/com/saas/framework/utils/RegexUtils.java
@@ -5,8 +5,9 @@ import java.util.regex.Pattern;
 public class RegexUtils {
 
 	public static boolean phone(String phone) {
-		String pattern = "1[0-9]\\d{9}$";
-		return Pattern.matches(pattern, phone);
+		String pattern = "^1[\\d]{10}";
+		boolean flag = Pattern.matches(pattern, phone);
+		return flag;
 
 	}
 
